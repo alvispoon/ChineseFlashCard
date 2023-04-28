819b081b1971012e8eb3d07fc2304169d9dcb527
Poon Tsz Kin <alvispoon@gmail.com>
2023-04-28 13:15:22 +0800

    Initial Commit

---
+++ ChineseFlashCard/ChineseFlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseFlashCardApp.swift
+//  ChineseFlashCard
+//
+//  Created by Alvis Poon on 28/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChineseFlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
